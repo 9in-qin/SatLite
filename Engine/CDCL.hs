8fa938c8ec14841d921b7da6181c5ba582770b97
9in-qin <qin_9in@outlook.com>
2026-05-18 13:59:50 +1000

    clean up CDCL.hs

--- Engine/CDCL.hs
+++ Engine/CDCL.hs
@@ -22,31 +22,35 @@ data Result = UNSAT | SAT (ClauseDB, SolverState) deriving (Show)
 cdcl :: ClauseDB -> SolverState -> Result
 cdcl db ss =
     let propagation = propagate db ss
-    in case hasConflict propagation of
-        (db', ss', Just cid) -> case currentLevel $ trail ss' of
+    in case propagation of
+        (db', ss', DoesConflict cid) -> case currentLevel $ trail ss' of
             0  -> UNSAT
             lv -> let (newDB, newSS) = processConflict lv cid db' ss'
                   in cdcl newDB newSS
-        (db', ss', Nothing) -> case mostActiveVar (varCount db') (assignment ss') (varActivity ss') of
+        (db', ss', NoConflict) -> case mostActiveVar (varCount db') (assignment ss') (varActivity ss') of
             Nothing      -> SAT (db', ss')
-            Just nextVar -> let ss0 = ss' { assignment = IntMap.insert (getVar nextVar) True $ assignment ss'
-                                          , queue = enqueue (varToLit nextVar) $ queue ss'
-                                          , trail = trailPush (newLevel $ trail ss') (nextVar, Decided)
-                                          }
-                            in cdcl db' ss0
-
-hasConflict :: (ClauseDB, SolverState, IfConflict) -> (ClauseDB, SolverState, Maybe CID)
-hasConflict (db, ss, NoConflict) = (db, ss, Nothing)
-hasConflict (db, ss, DoesConflict cid) = (db, ss, Just cid)
+            Just nextVar -> let newSS = processDecide ss' nextVar
+                            in cdcl db' newSS
 
 processConflict :: Level -> CID -> ClauseDB -> SolverState -> (ClauseDB, SolverState)
-processConflict lv cid db ss =
-    let (learned, firstUIP) = analyze (extractInfo (db, ss, cid)) (clauses db)
+processConflict lv cid db ss = (newDB, newSS)
+    where
+        (learned, firstUIP) = analyze (extractInfo (db, ss, cid)) (clauses db)
         learnedClause       = IntMap.elems learned
         (newDB, backjumpSS) = backjump db ss learnedClause firstUIP lv
         newConflictCount    = conflictCount backjumpSS + 1
         newVarActivity      = conflictBasedUpdate newConflictCount (varActivity backjumpSS) learnedClause
-        updatedSS           = backjumpSS {varActivity = newVarActivity , conflictCount = newConflictCount}
-        newSS               = if newConflictCount == restartThreshold updatedSS
-                              then restart newDB updatedSS else updatedSS
-    in  (newDB, newSS)
+        updatedSS = backjumpSS { varActivity = newVarActivity
+                               , conflictCount = newConflictCount
+                               }
+                               
+        newSS = if newConflictCount == restartThreshold updatedSS
+                then restart newDB updatedSS
+                else updatedSS
+
+processDecide :: SolverState -> Var -> SolverState
+processDecide ss nextVar =
+    ss { assignment = IntMap.insert (getVar nextVar) True $ assignment ss
+       , queue = enqueue (varToLit nextVar) $ queue ss
+       , trail = trailPush (newLevel $ trail ss) (nextVar, Decided)
+       }
