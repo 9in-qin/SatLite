750edfa3d124b9e3f8fce254033ac314d825fdb5
9in-qin <qin_9in@outlook.com>
2026-05-19 18:39:35 +1000

    clean up assignment

--- Core/Assignment.hs
+++ Core/Assignment.hs
@@ -2,8 +2,8 @@ module Core.Assignment where
 
 import qualified Data.IntMap as IntMap
 
-import Core.Var
 import Core.Lit
+import Core.Var
 
 type Assignment = IntMap.IntMap Bool
 data LitType    = LitTrue | LitFalse | LitUnassigned
@@ -12,4 +12,6 @@ literalType :: Assignment -> Lit -> LitType
 literalType asgmt lit =
     case IntMap.lookup (getVar (litToVar lit)) asgmt of
         Nothing  -> LitUnassigned
-        Just val -> if ifLiteralTrue lit val then LitTrue else LitFalse
+        Just val -> if ifLiteralTrue lit val
+                    then LitTrue
+                    else LitFalse
