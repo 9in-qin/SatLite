85a86ec9fe1a33a6d3a2774dcc90c296d9a09936
9in-qin <qin_9in@outlook.com>
2026-05-20 22:34:17 +1000

    small improvement

--- Parser.hs
+++ Parser.hs
@@ -4,28 +4,41 @@ import Data.List
 
 parse :: String -> ([[Int]], (Int, Int))
 parse input = 
-        let
-            allLines = lines input
-            headerLine =
-                case find isHeaderLine allLines of
-                    Just line -> line
-                    Nothing   -> error "No header line found"
-            clauseLines = filter isClauseLine allLines
-        in  (map purify clauseLines, parseHeaderLine headerLine)
-        where
-            isHeaderLine :: String -> Bool
-            isHeaderLine line = "p cnf" `isPrefixOf` line
+    let allLines = lines input
+        headerLine =
+            case find isHeaderLine allLines of
+                Just line -> line
+                Nothing   -> error "No header line found"
+        clauseLines = map init $ filter clauseLineOnly allLines
+    in (map stringToInt clauseLines, parseHeaderLine headerLine)
+    where
+        isHeaderLine :: String -> Bool
+        isHeaderLine line =
+            "p cnf" `isPrefixOf` line
 
-            parseHeaderLine :: String -> (Int, Int)
-            parseHeaderLine line =
-                case words line of
-                    ("p":"cnf":numVars:numClauses:_) ->
-                        (read numVars, read numClauses)
-                    _                                ->
-                        error "Invalid header line format"
+        isCommentLine :: String -> Bool
+        isCommentLine line =
+            "c" `isPrefixOf` line
 
-            isClauseLine :: String -> Bool
-            isClauseLine line = not ("c" `isPrefixOf` line || "p cnf" `isPrefixOf` line)
+        isClauseLine :: String -> Bool
+        isClauseLine line =
+            "0" `isSuffixOf` line
+        
+        isEmptyLine :: String -> Bool
+        isEmptyLine "" = True
+        isEmptyLine _ = False
 
-            purify :: String -> [Int]
-            purify line = map read $ init $ words line
+        clauseLineOnly :: String -> Bool
+        clauseLineOnly line =
+            all ($ line) [isClauseLine, not . isCommentLine, not . isEmptyLine]
+
+        stringToInt :: String -> [Int]
+        stringToInt line =
+            map read (words line)
+
+        parseHeaderLine :: String -> (Int, Int)
+        parseHeaderLine line =
+            case words line of
+                ("p" : "cnf" : numVars : numClauses:_) ->
+                    (read numVars, read numClauses)
+                _ -> error "Invalid header line format"
